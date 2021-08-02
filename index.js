6a2faa5a208e431a5133cd74a921791ab3427196
cakelord-1870 <4300holdingsinc@gmail.com>
2021-08-02 10:26:53 -0400

    init

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const express = require('express');
-const app = express();
-app.use(express.static('public'));
-app.listen(3000);
+const express = require('express')
+const app = express()
+const port = 8080
+app.use(express.static('public'))
+app.listen(port)
 
 app.get('/', function(req, res) {
-    res.render('public/index.html');
+    res.render('public/index.html')
 });
