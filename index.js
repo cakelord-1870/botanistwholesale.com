2fbbf6daa1e3551cc359bf3a0959d0e1f9e41333
cakelord-1870 <4300holdingsinc@gmail.com>
2021-08-02 10:25:47 -0400

    init

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 app.use(express.static('public'));
-app.listen(port, () => { console.log('Example app listening at http://localhost:') })
+app.listen(3000);
+
+app.get('/', function(req, res) {
+    res.render('public/index.html');
+});
